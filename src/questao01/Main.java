f4048f4933f2606b4dd85f4ddd957970c80ba793
Rafael Rossi <raffarossi@hotmail.com>
2018-03-14 20:05:40 -0300

    Limitação de 10mil linhas e 200 caracteres por linha adicionado.

--- src/questao01/Main.java
+++ src/questao01/Main.java
@@ -1,18 +1,10 @@
 package questao01;
 
-//Rafael Bertoldi Rossi e Matheus Navarro Niewnow
+//Rafael Bertoldi Rossi e  Matheus Navarro Niewnow
 
-import java.awt.List;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -22,27 +14,34 @@ public class Main {
 		Set<String> colecaoSet = new TreeSet<String>();
 		String[] array;
 		try (BufferedReader br = new BufferedReader(new FileReader("C:/temp/teste.txt"))) {
-			StringBuilder sb = new StringBuilder();
 			String line = br.readLine();
+			int qtdLinhas = 0;
+			while ((line != null)) {
+				if (qtdLinhas <= 10000) {
+					if (line.length() <= 200) {
+						line = line.toLowerCase();
+						line = line.trim();
+						line = line.replaceAll("\\*", "");
+						line = line.replaceAll("\\(*", "");
+						line = line.replaceAll("\\$", "");
+						line = line.replaceAll("\\#", "");
+						line = line.replaceAll("\\.", "");
+						line = line.replaceAll("\\:", "");
+						line = line.replaceAll("\"", "");
+						array = line.split(" ");
+						line = line.replaceAll("\n", "");
+						for (String s : array) {
+							colecaoSet.add(s);
+						}
+						line = br.readLine();
+						qtdLinhas++;
+					} else
+						line = br.readLine();
+				} else
+					break;
 
-			while (line != null) {
-				line = line.toLowerCase();
-				line = line.trim();
-				line = line.replaceAll("\\*", "");
-				line = line.replaceAll("\\(*", "");
-				line = line.replaceAll("\\$", "");
-				line = line.replaceAll("\\#", "");
-				line = line.replaceAll("\\.", "");
-				line = line.replaceAll("\\:", "");
-				line = line.replaceAll("\"", "");
-				array = line.split(" ");
-				line = line.replaceAll("\n", "");
-				for (String s : array) {
-					colecaoSet.add(s);
-				}
-
-				line = br.readLine();
 			}
+
 			for (String s : colecaoSet) {
 				System.out.println(s);
 			}
